9a8abc4aa44290e117f0054c3b16736929ac4353
Denilson <denilson_contato@outlook.com>
2021-10-22 11:40:25 -0300

    Aula 182 - Predicate #2

---
+++ src/fundamentosJava/lambda/Predicado2.java
@@ -0,0 +1,17 @@
+package fundamentosJava.lambda;
+
+import java.util.function.Predicate;
+
+public class Predicado2 {
+
+	public static void main(String[] args) {
+
+		// Recebe um argumento e retorna um valor booleano
+		Predicate<Integer>  isPar = num -> num % 2 ==0;
+		Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+		
+		System.out.println(isPar.and(isTresDigitos).negate().test(123));
+		System.out.println(isPar.or(isTresDigitos).test(123));
+	}
+
+}
