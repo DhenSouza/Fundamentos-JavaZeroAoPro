c93c78fecad01feb62b2e1793408497969498c16
Denilson <denilson_contato@outlook.com>
2021-10-26 14:09:14 -0300

    Aula 188 - Interfaces Funcionais Antes JAVA 8

--- src/fundamentosJava/lambda/Desafio.java
+++ src/fundamentosJava/lambda/Desafio.java
@@ -15,38 +15,23 @@ public class Desafio {
 		 * 5. Formatar: R$1234,56
 		 **/
 
-		Produto2 produto1 = new Produto2("XBOX", 3235.80, 0.13);
 
-		Function<Produto2, Double> precoComDesconto = prod -> {
-			return (prod.getPreco() * (1 - prod.getDesconto()));
-		};
+		Function<Produto2, Double> precoComDesconto = prod -> 
+		(prod.getPreco() * (1 - prod.getDesconto()));;
 		
-		UnaryOperator<Double> impostoMunicipal = prod -> {
-			if(prod >= 2500) {
-				return  prod * 1.085;
-			} else if(prod < 2500) {
-				return prod;
-			}
-			return 0.;
-		};
 		
-		UnaryOperator<Double> frete = prod -> {
-			if(prod >= 3000) {
-				return prod + 100;
-			} else if(prod < 3000) {
-				return prod + 50;
-			}
-			return 0.;
-		};
-		
+		UnaryOperator<Double> impostoMunicipal = prod -> prod >= 2500 ? prod * 1.085 : prod;
+		UnaryOperator<Double> frete = prod -> prod >= 3000 ? prod + 100 : prod + 50;
 		UnaryOperator<Double> arredondar = prod -> Double.parseDouble(String.format("%.2f", prod));
-		
 		Function<Double, String> formatacao = preco -> ("R$" + preco).replace(".", ",");
 		
+		
+		Produto2 produto1 = new Produto2("XBOX", 3235.80, 0.50);
+		
 		String resultadoFinal = precoComDesconto
-			//	.andThen(impostoMunicipal)
-			//	.andThen(frete)
-			//	.andThen(arredondar)
+				.andThen(impostoMunicipal)
+				.andThen(frete)
+//				.andThen(arredondar)
 				.andThen(formatacao)
 				.apply(produto1);
 		
---
+++ src/fundamentosJava/lambda/Trabalho1.java
@@ -0,0 +1,18 @@
+package fundamentosJava.lambda;
+
+public class Trabalho1 implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Tarefa #1");
+			try {
+				Thread.sleep(100);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+}
